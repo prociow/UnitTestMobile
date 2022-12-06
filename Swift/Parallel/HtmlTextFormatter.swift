6c24d0cba790b4d8ecb743df3acb5c6a5d628c0d
prociow <prociow@gmail.com>
2022-12-06 20:50:19 +0100

    Create HtmlTextFormatter.swift

---
+++ Swift/Parallel/HtmlTextFormatter.swift
@@ -0,0 +1,25 @@
+class HtmlTextFormatter {
+public func FormatAsBold(text: String) -> String {
+return "<b>(text)</b>"
+}
+  
+public func FormatAsItalic(text: String) -> String {
+    return "<i>\(text)</i>"
+}
+
+public func FormatAsSmall(text: String) -> String {
+    return "<small>\(text)</small>"
+}
+
+public func FormatAsDeleted(text: String) -> String {
+    return "<del>\(text)</del>"
+}
+
+public func FormatAsSubscript(text: String) -> String {
+    return "<sub>\(text)</sub>"
+}
+
+public func FormatAsCustomTag(text: String, customTag: String) -> String {
+    return "<\(customTag)>\(text)</\(customTag)>"
+}
+}
