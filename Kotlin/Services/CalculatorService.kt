6e96a5cec66ebeac939915e5608af57b198c5cf6
prociow <prociow@gmail.com>
2022-12-06 21:23:32 +0100

    Create CalculatorService.kt

---
+++ Kotlin/Services/CalculatorService.kt
@@ -0,0 +1,19 @@
+class CalculatorService {
+  public fun Add(a: Int, b: Int): Int {
+    return a + b
+  }
+  public fun Sub(a: Int, b: Int): Int {
+    return a - b
+  }
+  public fun Mul(a: Int, b: Int): Int {
+    return a * b
+  }
+  
+  public fun Div(a: Int, b: Int): Int {
+    return a / b
+  }
+}
+
+
+
+
