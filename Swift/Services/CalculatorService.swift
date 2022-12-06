2a252046e87de64ca9fd3d6fa4a1cf32f2be6d89
prociow <prociow@gmail.com>
2022-12-06 20:55:04 +0100

    Create CalculatorService.swift

---
+++ Swift/Services/CalculatorService.swift
@@ -0,0 +1,19 @@
+class CalculatorService {
+
+  public func Add(a: Int, b: Int) -> Int {
+    return a + b
+  }
+  public func Sub(a: Int, b: Int) -> Int {
+    return a - b
+  }
+  public func Mul(a: Int, b: Int) -> Int {
+    return a * b
+  }
+  public func Div(a: Int, b: Int) -> Int {
+    return a / b
+  }
+}
+
+
+
+
